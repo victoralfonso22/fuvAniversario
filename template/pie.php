58f33d09d9b8dc3811c3b1fbdd5f8e2abf175e48
JuanHM <JuanHM@JuanHM-PC>
2017-02-22 17:28:06 -0600

    comienzo del footer e inserción de imágenes en él. Se eliminó el código del footer del index

--- template/pie.php
+++ template/pie.php
@@ -1,7 +1,39 @@
-<?php
+                                <div id="footer-wrapper" class="wrapper">					
+					<div id="footer" class="container">						
+						<div>							
+                                                        <div>
 
-/* 
- * Pie de página
- */
+								<!-- Contact -->                                                                        
+									<section>                                                                                
+                                                                            <table>
+                                                                                <thead><h3>Contáctanos</h3><p>Te canalizaremos al área que corresponda</p></thead>
+                                                                                <tr>
+                                                                                    <td><p><img src="./assets/css/images/icon-mail.png">&nbsp; contacto@fundacionuv.org.mx</p></td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td><p><img src="./assets/css/images/icon-tel.png">&nbsp; Teléfonos:<br />01-800-838-4181<br />+52 (228) 841 59 22</p></td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td><img src="./assets/css/images/icon-map.png">&nbsp;Calle Veracruz No 46 int 1 y 2<br />Fraccionamiento Pomona<br />C.P. 91040 Xalapa, Veracruz</td>
+                                                                                </tr>
+                                                                            </table>
+									</section>
 
-/*etsito compi*/
+							</div>
+						</div>						
+					</div>					
+				</div>
+                </div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
