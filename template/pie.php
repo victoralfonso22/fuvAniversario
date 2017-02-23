85cd1eca08e447909b5f526ce427c0be9f27570f
Victor Alfonso Quitano Huesca <victoralfonso22@gmail.com>
2017-02-23 10:05:04 -0600

    ultima modificacion para que Juan baje de nuevo el proyecto

--- template/pie.php
+++ template/pie.php
@@ -11,16 +11,16 @@
                                                                                     <td><h3>Unidad de transparencia</h3></td>
                                                                                 </thead>                                                                                
                                                                                 <tr>
-                                                                                    <td><p><img src="./assets/css/images/icon-mail.png">&nbsp; contacto@fundacionuv.org.mx</p></td>
+                                                                                    <td><p><img src="http://localhost/fuvAniversario/assets/css/images/icon-mail.png">&nbsp; contacto@fundacionuv.org.mx</p></td>
                                                                                     <td><b>Órganos de gobierno</b><br />Asamblea General de Asociados<br />Junta Directiva</td>
                                                                                     <td rowspan="2"><b>Obligaciones</b><br /><b>Acreditaciones y certificaciones</b><br /><b>Aviso de Privacidad</b><br /></td>
                                                                                 </tr>
                                                                                 <tr>
-                                                                                    <td><p><img src="./assets/css/images/icon-tel.png">&nbsp; Teléfonos:<br />01-800-838-4181<br />+52 (228) 841 59 22</p></td>
+                                                                                    <td><p><img src="http://localhost/fuvAniversario/assets/css/images/icon-tel.png">&nbsp; Teléfonos:<br />01-800-838-4181<br />+52 (228) 841 59 22</p></td>
                                                                                     <td><b>Comités Regionales de Becas (Voluntarios)</b><br />Coatzacoalcos - Minatitlán<br />Veracruz - Boca del Río<br />Xalapa</td>
                                                                                 </tr>
                                                                                 <tr>
-                                                                                    <td><img src="./assets/css/images/icon-map.png">&nbsp;Calle Veracruz No 46 int 1 y 2<br />Fraccionamiento Pomona<br />C.P. 91040 Xalapa, Veracruz</td>
+                                                                                    <td><img src="http://localhost/fuvAniversario/assets/css/images/icon-map.png">&nbsp;Calle Veracruz No 46 int 1 y 2<br />Fraccionamiento Pomona<br />C.P. 91040 Xalapa, Veracruz</td>
                                                                                     <td><h2>Órganos de gobierno</h2></td>
                                                                                     <td>Informe de labores de agosto 2015 a septiembre 2016</td>
                                                                                 </tr>
@@ -34,13 +34,13 @@
 
 		<!-- Scripts -->
 
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/skel-viewport.min.js"></script>
-			<script src="assets/js/util.js"></script>
+			<script src="http://localhost/fuvAniversario/assets/js/jquery.min.js"></script>
+			<script src="http://localhost/fuvAniversario/assets/js/jquery.dropotron.min.js"></script>
+			<script src="http://localhost/fuvAniversario/assets/js/skel.min.js"></script>
+			<script src="http://localhost/fuvAniversario/assets/js/skel-viewport.min.js"></script>
+			<script src="http://localhost/fuvAniversario/assets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
+			<script src="http://localhost/fuvAniversario/assets/js/main.js"></script>
 
 	</body>
 </html>
