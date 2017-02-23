0243f5922011466dce4ed833a556e049c35666e0
JuanHM <JuanHM@JuanHM-PC>
2017-02-22 18:07:39 -0600

    Progreso del footer e imágenes del mismo

--- template/pie.php
+++ template/pie.php
@@ -2,23 +2,30 @@
 					<div id="footer" class="container">						
 						<div>							
                                                         <div>
-
 								<!-- Contact -->                                                                        
 									<section>                                                                                
                                                                             <table>
-                                                                                <thead><td><h3>Contáctanos</h3><p>Te canalizaremos al área que corresponda</p></td></thead>
+                                                                                <thead>
+                                                                                    <td><h3>Contáctanos</h3><p>Te canalizaremos al área que corresponda</p></td>
+                                                                                    <td><h3>Organización y estructura</h3></td>
+                                                                                    <td><h3>Unidad de transparencia</h3></td>
+                                                                                </thead>                                                                                
                                                                                 <tr>
                                                                                     <td><p><img src="./assets/css/images/icon-mail.png">&nbsp; contacto@fundacionuv.org.mx</p></td>
+                                                                                    <td><b>Órganos de gobierno</b><br />Asamblea General de Asociados<br />Junta Directiva</td>
+                                                                                    <td rowspan="2"><b>Obligaciones</b><br /><b>Acreditaciones y certificaciones</b><br /><b>Aviso de Privacidad</b><br /></td>
                                                                                 </tr>
                                                                                 <tr>
                                                                                     <td><p><img src="./assets/css/images/icon-tel.png">&nbsp; Teléfonos:<br />01-800-838-4181<br />+52 (228) 841 59 22</p></td>
+                                                                                    <td><b>Comités Regionales de Becas (Voluntarios)</b><br />Coatzacoalcos - Minatitlán<br />Veracruz - Boca del Río<br />Xalapa</td>
                                                                                 </tr>
                                                                                 <tr>
                                                                                     <td><img src="./assets/css/images/icon-map.png">&nbsp;Calle Veracruz No 46 int 1 y 2<br />Fraccionamiento Pomona<br />C.P. 91040 Xalapa, Veracruz</td>
+                                                                                    <td><h2>Órganos de gobierno</h2></td>
+                                                                                    <td>Informe de labores de agosto 2015 a septiembre 2016</td>
                                                                                 </tr>
                                                                             </table>
 									</section>
-
 							</div>
 						</div>						
 					</div>					
