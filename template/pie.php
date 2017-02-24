11ed76756b6335871c465da5812864ef5d31f644
JuanHM <JuanHM@JuanHM-PC>
2017-02-24 12:59:44 -0600

    footer armado

--- template/pie.php
+++ template/pie.php
@@ -1,40 +1,54 @@
                                 <div id="footer-wrapper" class="wrapper">					
-					<div id="footer" class="container">						
-						<div>							
-                                                        <div>
-								<!-- Contact -->                                                                        
-									<section>                                                                                
-                                                                            <table>
-                                                                                <thead>
-                                                                                    <td><h3>Contáctanos</h3><p>Te canalizaremos al área que corresponda</p></td>
-                                                                                    <td><h3>Organización y estructura</h3></td>
-                                                                                    <td><h3>Unidad de transparencia</h3></td>
-                                                                                </thead>                                                                                
-                                                                                <tr>
-                                                                                    <td><p><img src="http://localhost/fuvAniversario/assets/css/images/icon-mail.png">&nbsp; contacto@fundacionuv.org.mx</p></td>
-                                                                                    <td><b>Órganos de gobierno</b><br />Asamblea General de Asociados<br />Junta Directiva</td>
-                                                                                    <td rowspan="2"><b>Obligaciones</b><br /><b>Acreditaciones y certificaciones</b><br /><b>Aviso de Privacidad</b><br /></td>
-                                                                                </tr>
-                                                                                <tr>
-                                                                                    <td><p><img src="http://localhost/fuvAniversario/assets/css/images/icon-tel.png">&nbsp; Teléfonos:<br />01-800-838-4181<br />+52 (228) 841 59 22</p></td>
-                                                                                    <td><b>Comités Regionales de Becas (Voluntarios)</b><br />Coatzacoalcos - Minatitlán<br />Veracruz - Boca del Río<br />Xalapa</td>
-                                                                                </tr>
-                                                                                <tr>
-                                                                                    <td><img src="http://localhost/fuvAniversario/assets/css/images/icon-map.png">&nbsp;Calle Veracruz No 46 int 1 y 2<br />Fraccionamiento Pomona<br />C.P. 91040 Xalapa, Veracruz</td>
-                                                                                    <td><h2>Órganos de gobierno</h2></td>
-                                                                                    <td>Informe de labores de agosto 2015 a septiembre 2016</td>
-                                                                                </tr>
-                                                                            </table>
-									</section>
+					
+                                            <!-- Highlights -->
+                                            					
+                                                <div id="highlights" class="container">
+                                                    <div class="row 50%">
+							<div class="4u 12u(mobile)">
+								<section class="highlight">									
+                                                                        <h3>Contáctanos<br/><span style="font-size:14px">Te canalizaremos al área que corresponda</span></h3>
+                                                                        <p><img src="./assets/css/images/icon-mail.png"/><span style="margin-left:4%">contacto@fundacionuv.org.mx</span><br />
+                                                                           <img style="padding-top:5%" src="./assets/css/images/icon-tel.png"/><span style="margin-left:6%">Teléfonos:</span><br /><a style="margin-left:15%" href="tel:018008384181">01-800-838-4181</a><br /><a style="margin-left:15%" href="tel:522288415922">+52 (228) 841 59 22</a><br />
+                                                                           <img src="./assets/css/images/icon-map.png"/><span style="margin-left:6%">Calle Veracruz No 46 Int 2 y 3</span><br /><span style="margin-left:14%">Fraccionamiento Pomona</span><br /><span style="margin-left:14%">C.P. 91040 Xalapa, Veracruz</span></p>                                                                        
+								</section>
 							</div>
-						</div>						
-					</div>					
-				</div>
+							<div class="4u 12u(mobile)">
+								<section class="highlight">									
+									<h3>Organización y estructura</h3>
+                                                                        <p><b>Órganos de gobierno</b><br />Asamblea General de Asociados</p>
+                                                                        <p><b>Comités Regionales de Becas(Voluntarios)</b><br />Coatzacoalcos - Minatitlán<br />Veracruz - Boca del Río<br />Xalapa</p>
+                                                                        <p><b>Comités Regionales de Becas(Voluntarios)</b></p>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section class="highlight">									
+									<h3>Unidad de transparencia</h3><br/>
+									<p><a><b>Obligaciones</b></a><br /><br/><a><b>Derechos ARCO</b></a><br/><br/><a><b>Acreditaciones y certificaciones</a></b><br/><br/><a><b>Aviso de privacidad</b></a></p>
+                                                                        <a><h3>Informe de labores</h3></a>
+								</section>
+							</div>
+                                                    </div>
+                                                </div>  
+                                            <div id="copyright">
+                                    <table style="width:75%; margin: 00;">
+                                        <tr>
+                                            <td style="vertical-align:middle;text-align:right;">
+                                                <span style="color:black;">Todos los derechos reservados© 2017 Fundación de la Universidad Veracruzana, A.C.</span>
+                                            </td>
+                                            <td>
+                                                <img src="./assets/css/images/logo-UV.png"/>
+                                            </td>
+                                            <td>
+                                                <img src="./assets/css/images/logo-semefi.png"/>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+				</div>                                
                 </div>
 
 		<!-- Scripts -->
 
-			<script src="http://localhost/fuvAniversario/assets/js/jquery.min.js"></script>
 			<script src="http://localhost/fuvAniversario/assets/js/jquery.dropotron.min.js"></script>
 			<script src="http://localhost/fuvAniversario/assets/js/skel.min.js"></script>
 			<script src="http://localhost/fuvAniversario/assets/js/skel-viewport.min.js"></script>
