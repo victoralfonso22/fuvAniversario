b017a92d99370553af9ca056b174322bf1a2cce7
Victor Alfonso Quitano Huesca <victoralfonso22@gmail.com>
2017-03-01 18:49:03 -0600

    Se crea la seccion de acceso rapido en el home y se empieza con la siguiente seccion de "Lo mas reciente"

--- index.php
+++ index.php
@@ -1,103 +1,93 @@
-			<!-- Header -->
-			<?php include('template/cabecera.php'); ?>
-                        
-                        <!-- Implementacion del slider 01-03-2017 by viko -->
-                        <link rel="stylesheet" href="assets/css/glide.core.css" />
-                        <link rel="stylesheet" href="assets/css/glide.theme.css" />
-                        <script src="assets/js/glide.js"></script>
-			<!-- Slider -->
-                        <div id="div100Slider">
-                        <div id="divSlider" class="container">                            
-                        <div id="Glide" class="glide">
+<!-- Header -->
+<?php include('template/cabecera.php'); ?>
 
-                            <div id="flechasSlider" class="glide__arrows">
-                                <button class="glide__arrow prev" data-glide-dir="<"><img src="assets/css/images/Flacha-left.png"></button>
-                                <button class="glide__arrow next" data-glide-dir=">"><img src="assets/css/images/Flacha-right.png"></button>
-                            </div>
+<!-- Implementacion del slider 01-03-2017 by viko -->
+<link rel="stylesheet" href="assets/css/glide.core.css" />
+<link rel="stylesheet" href="assets/css/glide.theme.css" />
+<link rel="stylesheet" href="assets/css/least.min.css" />
+<script src="assets/js/glide.js"></script>
+<script src="assets/js/least.min.js"></script>
+<!-- Slider -->
+<div id="div100Slider">
+    <div id="divSlider" class="container">                            
+        <div id="Glide" class="glide">
 
-                            <div class="glide__wrapper">
-                                <ul class="glide__track">
-                                    <li class="glide__slide"><img id="imgSlider" src="imagenes/carrusel-1.jpg" /></li>
-                                    <li class="glide__slide"><img id="imgSlider" src="imagenes/carrusel-2.jpg" /></li>
-                                    <li class="glide__slide"><img id="imgSlider" src="imagenes/carrusel-3.jpg" /></li>
-                                </ul>
-                            </div>
+            <div id="flechasSlider" class="glide__arrows">
+                <button class="glide__arrow prev" data-glide-dir="<"><img src="assets/css/images/Flacha-left.png"></button>
+                <button class="glide__arrow next" data-glide-dir=">"><img src="assets/css/images/Flacha-right.png"></button>
+            </div>
 
-                            <div id="puntosSlider" class="glide__bullets"></div>
+            <div class="glide__wrapper">
+                <ul class="glide__track">
+                    <li class="glide__slide"><img id="imgSlider" src="imagenes/carrusel-1.jpg" /></li>
+                    <li class="glide__slide"><img id="imgSlider" src="imagenes/carrusel-2.jpg" /></li>
+                    <li class="glide__slide"><img id="imgSlider" src="imagenes/carrusel-3.jpg" /></li>
+                </ul>
+            </div>
 
-                        </div>
-                        </div>
-                        </div>
-                        
-			<!-- Main -->
-				<div class="wrapper style2">
-					
-					<div id="main" class="container">
-                                            <header class="style1">
-                                                <h2>¿Qué estás buscando?</h2>
-                                                <div class="bordeAbajo"/>
-                                            </header>
+            <div id="puntosSlider" class="glide__bullets"></div>
 
-						<!-- Features -->
-							<section id="features">
+        </div>
+    </div>
+</div>
 
-								<div class="feature-list">
-									<div class="row">
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-comment">Mattis velit diam vulputate</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-refresh">Lorem ipsum dolor sit veroeros</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-									<div class="row">
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-picture-o">Pretium phasellus justo lorem</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-cog">Tempus sed pretium orci</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-									<div class="row">
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-wrench">Aliquam consequat et feugiat</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-check">Dolore laoreet aliquam mattis</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-								</div>
-								<ul class="actions actions-centered">
-									<li><a href="#" class="button style1 big">Get Started</a></li>
-									<li><a href="#" class="button style2 big">More Info</a></li>
-								</ul>
-							</section>
+<!-- Accesos rapidos 01-03-2017 by viko -->
+<div class="wrapper style2">
 
-					</div>
-				</div>			
+    <div id="main" class="container">
+        <header class="style1">
+            <h2>¿Qué estás buscando?</h2>
+            <div class="bordeAbajo"/>
+        </header>
+        <div class="container">
+            <table id="tabla">
+                <tr>
+                    <td >
+                        <a target="_blank" href="https://twitter.com/FundacionUV"><img src="imagenes/accesosRapidos/action beca.png" onmouseover="this.src = 'imagenes/accesosRapidos/action beca-hover.png'" onmouseout="this.src = 'imagenes/accesosRapidos/action beca.png'"></a>
+                    </td>
+                    <td >
+                        <a target="_blank" href="https://twitter.com/FundacionUV"><img src="imagenes/accesosRapidos/action proyecto.png" onmouseover="this.src = 'imagenes/accesosRapidos/action proyecto-hover.png'" onmouseout="this.src = 'imagenes/accesosRapidos/action proyecto.png'"></a>
+                    </td>
+                    <td>
+                        <a target="_blank" href="https://twitter.com/FundacionUV"><img src="imagenes/accesosRapidos/action renta.png" onmouseover="this.src = 'imagenes/accesosRapidos/action renta-hover.png'" onmouseout="this.src = 'imagenes/accesosRapidos/action renta.png'"></a>
+                    </td>
+                    <td>
+                        <a target="_blank" href="https://twitter.com/FundacionUV"><img src="imagenes/accesosRapidos/action lego.png" onmouseover="this.src = 'imagenes/accesosRapidos/action lego-hover.png'" onmouseout="this.src = 'imagenes/accesosRapidos/action lego.png'"></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td >
+                        Solicitar una beca
+                    </td>
+                    <td >
+                        Dar forma a mi proyecto
+                    </td>
+                    <td>
+                        Rentar un espacio para un evento
+                    </td>
+                    <td>
+                        Tomar un curso de Lego
+                    </td>
+                </tr>
+            </table>
+        </div>
 
-			<!-- Pié de página -->				
-                        <?php include('template/pie.php'); ?>		
-                        <script>
-                            $("#Glide").glide({
-                                type: "slideshow"
-                            });
-                        </script>
+
+    </div>
+</div>		
+    
+    <div id="mainReciente">
+        <div  class="container">
+        <header class="style1">
+            <h2>Lo más reciente</h2>
+            <div class="bordeAbajo"/>
+        </header>
+    </div>
+
+<!-- Pié de página -->				
+<?php include('template/pie.php'); ?>		
+<script>
+    $("#Glide").glide({
+        type: "slideshow"
+    });
+</script>
