ecca04625acb09be01551124d7301a890494a475
JuanHM <JuanHM@JuanHM-PC>
2017-03-21 13:19:28 -0600

    Footer/Organizacion y estructura y ajuste Dona a Becas Responsive

--- organizacion/index.php
+++ organizacion/index.php
@@ -1,50 +1,203 @@
+
 <!-- Header -->
 <?php include('../template/cabecera.php'); ?>
+<?php
+    if(isset($_GET['tipo'])){
+if($_GET['tipo'] == 1){
+    echo "<script>$(document).ready(function() {
+    $('#orgMenos').hide();
+    $('#orgMas').show();
+    $('html,body').animate({
+    scrollTop: $('#orgMas').offset().top
+    }, 1000);
+});</script>";
+}else if ($_GET['tipo'] == 2){
+    echo "<script>$(document).ready(function() {
+    $('#comiMenos').hide();
+    $('#comiMas').show();
+    $('html,body').animate({
+    scrollTop: $('#comiMas').offset().top
+    }, 1000);
+});</script>";
+}else if ($_GET['tipo'] == 3){
+    echo "<script>$(document).ready(function() {
+    $('#perMenos').hide();
+    $('#perMas').show();
+    $('html,body').animate({
+    scrollTop: $('#perMas').offset().top
+    }, 1000);
+});</script>";
+}
+}?>
+
 <div class="container">
-    <header class="style1">
-            <br/><h2>Organización y Estructura</h2>
-        <div class="bordeAbajo"></div>
-    </header>    
+<header class="style1">
+    <br/>
+<h2>Organización y estructura</h2>
+<div class="bordeAbajo"></div>
+</header>
+    
 </div>
-<div class="container">    
-    <!-- Accordion begin -->
-    <div class="accordion_example1">		
-        <!-- Section 1 -->
-	<div class="accordion_in acc_active">
-		<div class="acc_head">Órganos de gobierno</div>
-		<div class="acc_content">
-			<!-- Your text here. For this demo, the content is generated automatically. -->
-		</div>
-	</div>
-	<!-- Section 2 -->
-	<div class="accordion_in">
-		<div class="acc_head">Comités Regionales de Becas(Voluntarios)</div>
-		<div class="acc_content">
-				<!-- Your text here. For this demo, the content is generated automatically. -->
-		</div>
-	</div>
-	<!-- Section 3 -->
-	<div class="accordion_in">
-            <div class="acc_head">Personal Directivo</div>
-            <div class="acc_content">
-				<!-- Your text here. For this demo, the content is generated automatically. -->
-            </div>
-	</div>
+<div id="contenedorBlog" class="container">
+<div  class="container">
+    <div id="contenidoSub">
+    <!----------------    Texto para seccion Programa de becas     ----------------->
+    <p>Tú puedes apoyar el mantenimiento de los programas que ofrece la Fundación UV a través de la renta de alguno de nuestros espacios para tus eventos musicales, proyectos culturales, deportivos o académicos. Conócelos y elige el que se adapte mejor a tus necesidades.</p>
+    <div id="orgMenos" class="encabezadoSub">
+        <img class="puntero" onclick="mostrarOcultar('orgMas','orgMenos');" style="vertical-align: middle; padding-top: .3em;" src="img/desplegar icon.png">&nbsp;&nbsp;<label>Órganos de gobierno</label>
     </div>
-    <!-- Accordion end -->
+    <div id="orgMas" style="display: none">
+    <div id="divSubMenuNota">
+
+    <div class="encabezadoSub">
+        <img class="puntero" onclick="mostrarOcultar('orgMenos','orgMas');" style="vertical-align: middle; padding-top: .3em;" src="img/encoger-icon.png">&nbsp;&nbsp;<label>Órganos de gobierno</label>
+    </div>
+            <div class="div1Notas">
+    <ul>
+        <li type= disc>Asamblea General de Asociados
+            <ul>                
+                <li type= disc><b>Junta Directiva</b>
+                    <ul>
+                        <li type= disc>Mtro. Víctor Aguilar Pizarro – Presidente</li>
+                        <li type= disc>Ing. Jesús Antonio López Rosas – Vicepresidente</li>
+                        <li type= disc>Lic. Luis Rafael Hinojosa Moreno – Secretario</li>
+                        <li type= disc>Mtra. Enriqueta Sarabia Ramírez – Tesorera</li>                        
+                    </ul>
+                </li>
+                <li type= disc>Arq. Arturo  Aguayo Lozano – Asociado</li>
+                <li type= disc>LAE Sergio Benítez Obeso – Asociado</li>                
+                <li type= disc>Mtra. María Cristina Domínguez Villegas – Asociada</li>
+                <li type= disc>Arq. Antonio de Jesús Vilchis López - Asociado Fundador</li>
+                <li type= disc>Sr. Carlos Soto Díaz - Asociado Fundador</li>
+                <li type= disc>C.P. Rodrigo C. Campos Reyeros - Asociado y Consejero Estatal de Becas</li>
+                <li type= disc>Lic. Francisco Javier Escalera Navarrete – Asociado</li>
+                <li type= disc>LAE Enrique Acosta Libreros – Asociado</li>
+                <li type= disc>Lic. Rafael de Huerta Manjarrez - Asociado y Consejero Estatal de Becas</li>
+                <li type= disc>Dr. Julián Sánchez Cortázar – Asociado</li>
+                <li type= disc>Dr. Mauro Loyo Varela - Asociado Fundado</li>
+                <li type= disc>Lic. Carlos Manuel Ferráez Matus – Asociado</li>
+                <li type= disc>Sr. Antonio Chedraui Mafud – Asociado</li>
+                <li type= disc>Ing. Manuel Cienfuegos García - Asociado</li>
+                <li type= disc>Lic. Salvador Arana Watty - Asociado </li>
+                <li type= disc>Lic. Pascual Castillo Moctezuma – Asociado</li>
+                <li type= disc>C.P. Raúl de la Fuente Polanco – Asociado</li>
+                <li type= disc>Lic. César Alejandro Guillermo Torres Flores - Asociado</li>
+            </ul>
+        
+        </li>
+                                   
+    </ul>    
+    </div> 
 </div>
-        <link rel="stylesheet" href="acordeon/smk-accordion.css" />
-        <script type="text/javascript" src="acordeon/smk-accordion.js"></script>
-        <script type="text/javascript" src="acordeon/acordeon.js"></script>
-	<script type="text/javascript">
-		jQuery(document).ready(function($){
 
-			$(".accordion_example1").smk_Accordion();		
+</div>
 
-			// Demo text. Let's save some space to make the code readable. ;)
-			$('.acc_content').not('.nodemohtml').html('<p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Fusce aliquet neque et accumsan fermentum. Aliquam lobortis neque in nulla  tempus, molestie fermentum purus euismod.</p>');
-			
-		});
-	</script>
+<div class="separadorSubmenu">
+    
+</div>
+   <div id="comiMenos" class="encabezadoSub">
+        <img class="puntero" onclick="mostrarOcultar('comiMas','comiMenos');" style="vertical-align: middle; padding-top: .3em;" src="img/desplegar icon.png">&nbsp;&nbsp;<label>Comités Regionales de Becas (voluntarios)</label>
+    </div>
+    <div id="comiMas" style="display: none">
+    <div id="divSubMenuNota">
+        <div class="div1">
+    <div class="encabezadoSub">
+        <img class="puntero" onclick="mostrarOcultar('comiMenos','comiMas');" style="vertical-align: middle; padding-top: .3em;" src="img/encoger-icon.png">&nbsp;&nbsp;<label>Comités Regionales de Becas (voluntarios)</label>
+    </div>
+    <ul>
+    <li type= disc><b>Coatzacoalcos- Minatitlán</b>
+            <ul>                                
+                <li type= disc>Lic. Jesús Salas Lizaur - Presidente</li>
+                <li type= disc>Lic. Erik Madrazo Lara - Secretario</li>                
+                <li type= disc>Dr. Jorge Alberto Andaverde Arredondo - Coordinador Regional</li>
+                <li type= disc>Arq. José Antonio Wilburn González - Vocal </li>
+                <li type= disc>Ing. Claudio Geraldo Velasco Martell - Vocal</li>
+                <li type= disc>C.P. José Daniel Ruiz Flores - Vocal</li>                
+            </ul>    
+    </li>
+    </ul>
+    <ul>
+    <li type= disc><b>Veracruz-Boca del Río</b>  
+            <ul>                                
+                <li type= disc>Sr. Antonio Chedraui Mafud - Presidente</li>
+                <li type= disc>Lic. Gustavo Sousa Escamilla - Vicepresidente</li>                
+                <li type= disc>Lic. Luis Rafael Hinojosa Moreno - Secretario </li>
+                <li type= disc>Ing. Rafael Ortiz de Zárate Vimbert - Tesorero </li>
+                <li type= disc>Dr. Alfonso Gerardo Pérez Morales - Coordinador Regional </li>
+                <li type= disc>Psic. Teresa de Jesús Ríos Linares - Enlace de la Presidencia</li>
+                <li type= disc>Dr. Carlos Lamothe Zavaleta - Enlace de la Coordinación Regional</li>
+                <li type= disc>Lic. Mary Carmen Hernández Pérez - Asistente de la Presidencia</li>
+                <li type= disc>Mtra. Martha Elena Aguirre Serena – Asistente de la Coordinación Regional </li>
+                <li type= disc>Dr. Jesús Joaquín Morales Burguet - Vocal</li>
+                <li type= disc>Q. Armando Staboli Belmont - Vocal</li>
+                <li type= disc>Lic. Adrián Maynes García - Vocal</li>
+                <li type= disc>Sr. Manuel Caramés Cota - Vocal</li>
+                <li type= disc>Lic. Fernando Ortiz González - Vocal</li>
+                <li type= disc>Ing. Ezequiel Guzmán Arango - Vocal</li>
+                <li type= disc>Sr. Gilberto de Jesús Bravo Torra - Vocal</li>
+                <li type= disc>Ing. Ángel Carrano Castro - Vocal</li>
+                <li type= disc>Lic. Jesús Antonio Muñoz de Cote Sampiery - Vocal</li>
+                <li type= disc>Ing. Marcos Orduña Alcocer - Vocal</li>
+                <li type= disc>Sr. Guillermo Bouchez Gómez - Vocal</li>
+                <li type= disc>Ing. Pedro Aguilar Pizarro - Vocal</li>
+                <li type= disc>Lic. Jorge Rafael CoffauKayser - Vocal</li>
+                <li type= disc>Ing. José Manuel Urreta Ortega - Vocal</li>
+                <li type= disc>Lic. Francisco Muñoz Ruiz - Vocal</li>
+                <li type= disc>Lic. Ignacio Reyna Carrera - Vocal</li>
+                <li type= disc>Ing. Francisco Malpica Valverde - Vocal</li>
+                <li type= disc>Ing. Baltazar Pazos Gómez - Vocal</li>
+                <li type= disc>Ing. Baltazar Pazos de la Torre - Vocal</li>
+                <li type= disc>Ing. Carlos Eduardo Ramírez Capo - Vocal</li>
+                <li type= disc>Dr. Armando López Guerrero - Vocal</li>                
+            </ul>    
+    </li>
+    </ul>
+    <ul>
+    <li type= disc><b>Xalapa</b>
+            <ul>                                
+                <li type= disc>Ing. Manuel Cienfuegos García - Presidente </li>
+                <li type= disc>Lic. Salvador Arana Watty - Tesorero</li>                                
+            </ul>    
+    </li>
+    </ul>
+    </div>
+</div>
+
+</div>
+
+<div class="separadorSubmenu">
+    
+</div>
+    <div id="perMenos" class="encabezadoSub">
+        <img class="puntero" onclick="mostrarOcultar('perMas','perMenos');" style="vertical-align: middle; padding-top: .3em;" src="img/desplegar icon.png">&nbsp;&nbsp;<label>Personal Directivo</label>
+    </div>
+    <div id="perMas" style="display: none">
+    <div id="divSubMenuNota">
+        <div class="div1Notas">
+    <div class="encabezadoSub">
+        <img class="puntero" onclick="mostrarOcultar('perMenos','perMas');" style="vertical-align: middle; padding-top: .3em;" src="img/encoger-icon.png">&nbsp;&nbsp;<label>Personal Directivo</label>
+    </div>
+    <ul>
+                <li type= disc>Ing. Facundo Enrique Pacheco Rojas – Director General</li>
+                <li type= disc>C.P. Ángeles Contreras Meneses – Directora de Administración y Control</li>
+                <li type= disc>Mtro. Mauricio García Velázquez – Director de Desarrollo</li>
+    </ul>    
+    </div>
+        
+    <div id="paddinFonfo" />
+    </div>  
+    
+    
+      
+</div>
+
+</div>          
+    
+</div>
+</div>
+    </div>
+        
+<!-- contenido -->
+
 <!-- Pié de página -->				
 <?php include('../template/pie.php'); ?>
--- template/pie.php
+++ template/pie.php
@@ -5,18 +5,18 @@
                                                     <div class="row 50%">
 							<div class="4u 12u(mobile)">
 								<section class="highlight">									
-                                                                        <h3>Contáctanos<br/><span style="font-size:14px">Te canalizaremos al área que corresponda</span></h3>
+                                                                        <h3>Contáctanos<br/><!--<span style="font-size:14px">Te canalizaremos al área que corresponda</span>--></h3>
                                                                         <p><img src="http://localhost/fuvAniversario/assets/css/images/icon-mail.png"/><span style="margin-left:4%">contacto@fundacionuv.org.mx</span><br />
                                                                            <img style="padding-top:5%" src="http://localhost/fuvAniversario/assets/css/images/icon-tel.png"/><span style="margin-left:6%">Teléfonos:</span><br /><a style="margin-left:15%" href="tel:018008384181">01-800-838-4181</a><br /><a style="margin-left:15%" href="tel:522288415922">+52 (228) 841 59 22</a><br />
-                                                                           <img src="http://localhost/fuvAniversario/assets/css/images/icon-map.png"/><span style="margin-left:6%">Calle Veracruz No 46 Int 2 y 3</span><br /><span style="margin-left:14%">Fraccionamiento Pomona</span><br /><span style="margin-left:14%">C.P. 91040 Xalapa, Veracruz</span></p>                                                                        
+                                                                           <a href="https://www.google.com.mx/maps/place/Fundaci%C3%B3n+de+la+Universidad+Veracruzana+A.C./@19.5273502,-96.9103014,18z/data=!4m5!3m4!1s0x0:0x4ab4a54c408f391c!8m2!3d19.5281342!4d-96.9074573" target="_blank"><img src="http://localhost/fuvAniversario/assets/css/images/icon-map.png"/><span style="margin-left:6%">Calle Veracruz No 46 Int 2 y 3</span><br /><span style="margin-left:14%">Fraccionamiento Pomona</span><br /><span style="margin-left:14%">C.P. 91040 Xalapa, Veracruz</span></a></p>
 								</section>
 							</div>
 							<div class="4u 12u(mobile)">
 								<section class="highlight">									
 									<h3>Organización y estructura</h3>
-                                                                        <p><b>Órganos de gobierno</b><br />Asamblea General de Asociados</p>
-                                                                        <p><b>Comités Regionales de Becas(Voluntarios)</b><br />Coatzacoalcos - Minatitlán<br />Veracruz - Boca del Río<br />Xalapa</p>
-                                                                        <p><b>Personal Directivo</b></p>
+                                                                        <p><b><a href="http://localhost/fuvAniversario/organizacion/?tipo=1" style="color:#73d2d3">Órganos de gobierno</a></b><br />Asamblea General de Asociados</p>
+                                                                        <p><b><a href="http://localhost/fuvAniversario/organizacion/?tipo=2" style="color:#73d2d3">Comités Regionales de Becas(Voluntarios)</a></b><br />Coatzacoalcos - Minatitlán<br />Veracruz - Boca del Río<br />Xalapa</p>
+                                                                        <p><b><a href="http://localhost/fuvAniversario/organizacion/?tipo=3" style="color:#73d2d3">Personal Directivo</a></b></p>
 								</section>
 							</div>
 							<div class="4u 12u(mobile)">
@@ -29,50 +29,21 @@
                                                     </div>
                                                 </div>                                              
 			</div>   
-                        <div id="footer-wrapper" style="margin-top: -8%; margin-bottom:0%;">										
-                                            <!-- Highlights -->                                            					                                             
-                                            <div id="copyright">                                  
-                                                <div class="row 2%">
-                                                        <div class="6u 12u(mobile)">
-								<!-- Contact Form -->
-									<section>										
-											<div class="row -5%">												
-												<div class="6u">													
-												</div>
-											</div>
-											<div class="row 50%">
-												<div class="12u">
-													<span style="color:black;">Todos los derechos reservados© 2017 Fundación de la Universidad Veracruzana, A.C.</span>
-												</div>
-											</div>
-											<div class="row">
-												<div class="6u">													
-												</div>
-                                                                                            <div class="6u">													
-												</div>
-											</div>										
-									</section>
-							</div>
-							<div class="6u 12u(mobile)">
-								<!-- Contact -->
-									<section class="feature-list small" style="text-decoration: none;">
-										<div class="row">
-											<div class="6u 12u(mobile)">												
-                                                                                            <span><img src="http://localhost/fuvAniversario/assets/css/images/logo-UV.png"/></span>
-											</div>
-											<div class="6u 12u(mobile)">												
-                                                                                            <span><img src="http://localhost/fuvAniversario/assets/css/images/logo-semefi.png"/></span>
-											</div>
-										</div>										
-									</section>
-							</div>
-						</div>
-                                </div>                                                                
+                        <div id="footer-wrapper" style="margin-top: 0%; margin-bottom:0%; background: #fff; color: #000; padding-top:1%; padding-bottom: 1%">										
+                                            <!-- Highlights -->                                                                                                                                           
+                                                    <table>
+                                                        <tr>
+                                                            <td style="text-align: right; padding-right:5%; vertical-align: middle" width="50%">Todos los derechos reservados© 2017 Fundación de la Universidad Veracruzana, A.C.</td>
+                                                            <td width="10%"><img src="http://localhost/fuvAniversario/assets/css/images/logo-UV.png"/></td>
+                                                            <td width="20%"><img src="http://localhost/fuvAniversario/assets/css/images/logo-semefi.png"/></td>
+                                                        </tr>
+                                                    </table>                                                        													
+                                                                                              
 			</div> 
 <!-- versión móvil -->
                         <div id="footer-wrapper2">										
                                             <!-- Highlights -->                                            					
-                                            <div id="highlights" class="container" style="margin-bottom: 7%;">
+                                            <div id="highlights" class="container" style="margin-bottom: 7%; margin-left:-5%;">
                                                     <div class="row 50%">
 							<div class="4u 12u(mobile)">
 								<section class="highlight">									
